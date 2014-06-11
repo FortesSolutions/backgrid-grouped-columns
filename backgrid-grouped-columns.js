952c765c93c8cab1680c0702cd2f443812fd7238
pseeweb <wilbert.ridder@gmail.com>
2014-06-11 22:09:51 +0200

    - Changed from a top-down to a bottom up approach - Still supports top-down tree column definition - Column sort is now leading - Updated event binding - Saves headerRows for usage by other extensions

--- backgrid-grouped-columns.js
+++ backgrid-grouped-columns.js
@@ -16,12 +16,14 @@
 
 }(this, function (_, Backgrid) {
   var groupedHeader = Backgrid.Extension.GroupedHeader = Backgrid.Header.extend({
-    columnLayout: {},
+    columnLayout: null,
     columnCollection: Backgrid.Columns,
+    headerRows: [],
 
     initialize: function() {
       Backgrid.Header.prototype.initialize.apply(this, arguments);
-      this.attachEvents();
+      this.listenTo(this.columns, "change:renderable", this.render);
+      this.listenTo(this.columns, "add remove", this.render);
     },
 
     /**
@@ -30,80 +32,85 @@
     render: function () {
       var view = this;
       view.$el.empty();
-      var levelColumns = view.columnLayout;
-      var rowAmount = view.depthOf(levelColumns) / 2;
 
-      var columnFilter = function(arr, end) {
-          return _.filter(arr, function(obj) {
-            return (end) ? !_.has(obj, "children") : _.has(obj, "children");
-          });
-      };
-
-      var endCol = function(obj) {
-        levelColumns.push.apply(levelColumns, obj.children);
-      };
-
-      var createColDef = function(cols, level) {
-        var rowColDef = [];
-        _.each(levelColumns, function(obj) {
-          var columnModel = _.first(view.columns.where({ name: obj.name}));
-
-          // Check if it already has a column definition and if its renderable
-          var renderable = !columnModel || (typeof columnModel.get("renderable") == "undefined" || columnModel.get("renderable"));
-          renderable = (obj.children) ? renderable && view.hasValidChildren(obj) : renderable && _.isObject(columnModel);
-
-          if (renderable) {
-            var newObj = _.clone(obj);
-            var existingCol = view.columns.where({name: newObj.name});
-            newObj.attributes = {
-              colspan: view.countLeafs(newObj),
-              rowspan: (newObj.children) ? 1 : level
-            };
-            if (newObj.children) {
-              _.extend(newObj, {
-                sortable: false,
-                editable: false
-              });
-            }
-            rowColDef.push((_.isEmpty(existingCol)) ? newObj : _.extend(newObj, existingCol[0].toJSON()));
+      // If a column layout has been defined, determine nesting
+      if (view.columnLayout) {
+        var key;
+        for(key in view.columnLayout) {
+          if (view.columnLayout.hasOwnProperty(key)) {
+            view.calculateNesting(view.columnLayout[key]);
           }
-        });
-        return rowColDef;
-      };
-
-      var colLayoutNames = [];
-      var rows = [];
-      for (var i = rowAmount; i > 0; i--) {
-        // Get all columns on this level
-        var endColumns = columnFilter(levelColumns, true);
-        var nonEndColumns = columnFilter(levelColumns, false);
-
-        // Get all names of columns which have been defined in the column layout
-        colLayoutNames.push.apply(colLayoutNames, _.pluck(endColumns, "name"));
-
-        // Generate column definition for this row
-        rows.push(new view.columnCollection(createColDef(levelColumns, i)));
-
-        // Set columns for next level
-        levelColumns = [];
-        _.each(nonEndColumns, endCol);
+        }
       }
 
-      // Add columns which have not been defined in the column layout
-      view.columns.each(function(col) {
-        if (!_.contains(colLayoutNames, col.get("name"))) {
-          col.set("attributes", {colspan: 1, rowspan: rowAmount});
-          rows[0].add(col);
+      // Find amount of header rows
+      var rowAmount = view.findDepth();
+      var rows = Array.apply(null, new Array(rowAmount));
+      rows = rows.map(function () { return []; });
+
+      // Loop columns
+      var lastNesting = [];
+      view.columns.each(function(column) {
+        var colNesting = column.get("nesting");
+        var renderable = (typeof column.get("renderable") == "undefined" || column.get("renderable"));
+        if (colNesting && !_.isEmpty(colNesting) && renderable)  {
+          // Check for overlap and uniques with previous column; Use index based intersection
+          var overlap = _.filter(lastNesting, function(num, ind) { return num == colNesting[ind];});
+          var unique = _.difference(colNesting, overlap);
+
+          // Create unique parents
+          _.each(unique, function(element, index) {
+            rows[index + overlap.length].push({
+              name: element,
+              sortable: false,
+              editable: false,
+              attributes: {
+                colspan: 1,
+                rowspan: 1
+              },
+              childColumns: [column.get("name")]
+            });
+          });
+
+          // Increase colspan for every intersection
+          _.each(overlap, function(element, index) {
+            var lastElement = _.last(rows[index]);
+            lastElement.attributes.colspan++;
+            lastElement.childColumns.push(column.get("name"));
+          });
+
+          // Add main column
+          rows[colNesting.length].push(_.extend({
+            attributes: {
+              colspan: 1,
+              rowspan: rowAmount - colNesting.length
+            }}, column.toJSON()));
+
+          // Update nesting
+          lastNesting = colNesting;
+        }
+        else if(renderable) {
+          // Reset nesting
+          lastNesting = [];
+
+          // Create column definition attributes and add to rows
+          rows[0].push(_.extend({
+            attributes: {
+              colspan: 1,
+              rowspan: rowAmount
+          }}, column.toJSON()));
         }
       });
 
       // Render the rows
+      view.headerRows = [];
       _.each(rows, function(coll) {
         var row = new Backgrid.HeaderRow({
           columns: coll,
           collection: view.collection
         });
         view.$el.append(row.render().$el);
+        view.headerRows.push(row);
       });
 
       // Trigger event
@@ -112,55 +119,33 @@
       this.delegateEvents();
       return this;
     },
-    depthOf: function(object) {
-      var level = 1;
-      var key;
-
-      for(key in object) {
-        if (object.hasOwnProperty(key)) {
-          if(typeof object[key] == "object"){
-            var depth = this.depthOf(object[key]) + 1;
-            level = Math.max(depth, level);
-          }
-        }
-      }
-      return level;
-    },
-    countLeafs: function(object) {
-      var leafs = 0;
-
+    calculateNesting: function(object, nestArray) {
+      var nestingArray = _.clone(nestArray || []);
       if (_.has(object, "children") && _.isArray(object.children) && !_.isEmpty(object.children)) {
+        nestingArray.push(object.name);
         _.each(object.children, function(obj) {
-          leafs += this.countLeafs(obj);
+          this.calculateNesting(obj, nestingArray);
         }, this);
       }
       else {
         // No children, assume it's an existing column model
         var columnModel = _.first(this.columns.where({ name: object.name}));
         if (columnModel && (typeof columnModel.get("renderable") == "undefined" || columnModel.get("renderable"))) {
-          leafs += 1;
+          columnModel.set("nesting", nestingArray, {silent: true});
         }
       }
-      return leafs;
     },
-    hasValidChildren: function(object) {
-      var valid = false;
-      if (_.has(object, "children") && _.isArray(object.children) && !_.isEmpty(object.children)) {
-        _.each(object.children, function(obj) {
-          if (this.hasValidChildren(obj)) {
-            valid = true;
-          }
-        }, this);
-      }
-      else {
-        // No children, assume it's an existing column model
-        var columnModel = _.first(this.columns.where({ name: object.name}));
-        valid = (typeof columnModel.get("renderable") == "undefined" || columnModel.get("renderable"));
-      }
-      return valid;
-    },
-    attachEvents: function() {
-      this.listenTo(this.columns, "add remove change:renderable", this.render);
+    findDepth: function() {
+      var view = this;
+      var rows = 0;
+
+      view.columns.each(function(col) {
+        if (col.get('nesting')) {
+          rows = Math.max(rows, col.get('nesting').length);
+        }
+      });
+
+      return rows + 1;
     }
   });
 }));
